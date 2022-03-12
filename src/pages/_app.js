d1067531c285b4837943eb734826486d83b7b95a
Mohamed El Aammari <medlique@gmail.com>
2022-03-12 21:19:19 +0100

    added Google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,13 @@
 import Theme from '../styles/theme';
+import ReactGA from "react-ga"
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    ReactGA.initialize('G-PN4VRFQVWV');
+    //G-PN4VRFQVWV
+    ReactGA.pageview(window.location.pathname + window.location.search)
+    },[]);
   return (
     <>
       <Theme>
