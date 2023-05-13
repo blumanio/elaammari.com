839c96864fa9a51a33e4d97cc3ddd9f97f11669c
Mohamed El Aammari <moelaammari@gmail.com>
2023-05-13 21:46:42 +0200

    hide unfinished sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 // import Acomplishments from "../components/Acomplishments/Acomplishments";
 import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
-import Technologies from "../components/Technologies/Technologies";
-import Timeline from "../components/TimeLine/TimeLine";
+// import Technologies from "../components/Technologies/Technologies";
+// import Timeline from "../components/TimeLine/TimeLine";
 import { Layout } from "../layout/Layout";
 import { Section } from "../styles/GlobalComponents";
 
@@ -13,8 +13,8 @@ const Home = () => {
         <Hero />
       </Section>
       <Projects />
-      <Technologies />
-      <Timeline />
+      {/* <Technologies /> */}
+      {/* <Timeline /> */}
       {/* <Acomplishments /> */}
     </Layout>
   );
