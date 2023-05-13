8d4af619694d4b3dc3ec4d4c09d3ef53e423d0ed
Mohamed El Aammari <moelaammari@gmail.com>
2023-05-13 18:22:03 +0200

    add video
    added video background

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React from "react";
+import video1 from "../../public/videos/video1.mp4";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import { Container, Videobackground, Videowrapper } from "./LayoutStyles";
 
-import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
-import { Container } from './LayoutStyles'
-
-export const Layout = ({children}) => {
+export const Layout = ({ children }) => {
   return (
     <Container>
-     <Header/>
-     <main>{children}</main> 
-     <Footer/>
+      <Videowrapper>
+        <Videobackground>
+          <video src={video1} playsinline autoplay muted loop type="video/mp4">
+            <source src="/videos/video1.mp4" type="video/mp4"></source>
+            Your browser does not support the video tag.
+          </video>
+        </Videobackground>
+      </Videowrapper>
+
+      <Header />
+      <main>{children}</main>
+      <Footer />
     </Container>
-  )
-}
+  );
+};
--- src/layout/LayoutStyles.js
+++ src/layout/LayoutStyles.js
@@ -1,7 +1,25 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-max-width: 1280px;
-width: 100%;
-margin: auto;
+  //   max-width: 1280px;
+  width: 100%;
+  margin: auto;
+  background: red;
+`;
+export const Videobackground = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+export const Videowrapper = styled.div`
+  border: 2px solid #000;
+  width: 300px;
+  height: 300px;
+  position: relative;
+  overflow: hidden;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
